05eb388e4e7b6bfea87caabdcb2b08d792a661db
lab02-1660097 <datvu1303@gmail.com>
2018-12-09 12:23:51 +0700

    DTO

---
+++ DTO/DangKyNhom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DangKyNhom
+    {
+        private string _MSSV;
+        public string MSSV
+        {
+            get { return MSSV; }
+            set { _MSSV = value; }
+        }
+        private string _MANHOM;
+        public string MANHOM
+        {
+            get { return MANHOM; }
+            set { _MANHOM = value; }
+        }
+        public DangKyNhom(string MANHOM,string MSSV)
+        {
+            this._MANHOM = MANHOM;
+            this._MSSV = MSSV;
+        }
+
+
+    }
+}
