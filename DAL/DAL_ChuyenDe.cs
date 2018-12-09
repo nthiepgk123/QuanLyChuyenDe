c566d7e9b459a7e54955725def573593a39c647b
nthiepgk123 <44216571+nthiepgk123@users.noreply.github.com>
2018-12-09 11:51:27 +0700

    s

---
+++ DAL/DAL_ChuyenDe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace DAL
+{
+    class DAL_ChuyenDe : DBConnect
+    {
+        public DataTable Get()
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from ChuyenDe",_cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+    }
+}
--- DAL/DBConnect.cs
+++ DAL/DBConnect.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,6 @@ namespace DAL
 {
     class DBConnect
     {
+       protected SqlConnection _cn = new SqlConnection(@"DESKTOP-OUGNACU\SQLEXPRESS; Database=QuanLyHocVien; Trusted_Connection=True;");
     }
 }
