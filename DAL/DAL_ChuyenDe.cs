1030b040dbf1ffe81611ea48bc8ff11e2c67fa65
nthiepgk123 <44216571+nthiepgk123@users.noreply.github.com>
2018-12-09 12:30:32 +0700

    DAL

--- DAL/DAL_ChuyenDe.cs
+++ DAL/DAL_ChuyenDe.cs
@@ -5,7 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DAL;
+using DTO;
 
 namespace DAL
 {
@@ -26,5 +26,76 @@ namespace DAL
                 return null;
             }
         }
+
+        DataTable dt = new DataTable();
+        // Them Chuyen de
+        public bool Insert(ChuyenDe pChuyenDe)
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Select * from ChuyenDe", _cn);
+                DataRow r = new DataRow();
+                r["MaCD"] = pChuyenDe.MaChuyenDe;
+                r["MaNganh"] = pChuyenDe.MaNganh;
+                r["TenCD"] = pChuyenDe.TenChuyenDe;
+                r["SoHVMax"] = pChuyenDe.SoHocVienMax;
+
+                dt.Rows.Add(r);
+
+                SqlCommandBuilder cm = new SqlCommandBuilder(da);
+                da.Update(dt);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(string MaCD)
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from ChuyenDe", _cn);
+                DataRow r = dt.Rows.Find(MaCD);
+                if (r != null)
+                {
+                    r.Delete();
+                }
+                SqlCommandBuilder cm = new SqlCommandBuilder(da);
+                da.Update(dt);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Update(ChuyenDe pChuyenDe)
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from ChuyenDe", _cn);
+                DataRow r = dt.Rows.Find(pChuyenDe.MaChuyenDe);
+                if (r != null)
+                {
+                    r["MaNganh"] = pChuyenDe.MaNganh;
+                    r["TenCD"] = pChuyenDe.TenChuyenDe;
+                    r["SoHVMax"] = pChuyenDe.SoHocVienMax;
+                }
+                SqlCommandBuilder cm = new SqlCommandBuilder(da);
+                da.Update(dt);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
--- DTO/ChuyenDe.cs
+++ DTO/ChuyenDe.cs
@@ -8,6 +8,41 @@ namespace DTO
 {
     public class ChuyenDe
     {
+        private string _MaChuyenDe;
+        private string _MaNganh;
+        private string _TenChuyenDe;
+        private int _SoHocVienMax;
 
+        public string MaChuyenDe
+        {
+            get { return _MaChuyenDe; }
+            set { _MaChuyenDe = value; }
+        }
+
+        public string MaNganh
+        {
+            get { return _MaNganh; }
+            set { _MaNganh = value; }
+        }
+
+        public string TenChuyenDe
+        {
+            get { return _TenChuyenDe; }
+            set { _TenChuyenDe = value; }
+        }
+
+        public int SoHocVienMax
+        {
+            get { return _SoHocVienMax; }
+            set { _SoHocVienMax = value; }
+        }
+
+        public ChuyenDe(string MaCD , string MaNganh , string TenCD , int HVMax)
+        {
+            this._MaChuyenDe = MaCD;
+            this._MaNganh = MaNganh;
+            this._TenChuyenDe = TenCD;
+            this._SoHocVienMax = HVMax;
+        }
     }
 }
